1a53bce59d8f075a64d179587809a351f33b11cd
Jhlee <ljh990627@gmail.com>
2026-01-27 16:27:12 +0900

    히트 버튼, 스탠드 버튼 기능 구현

--- Assets/_Scripts/Cores/GameManager.cs
+++ Assets/_Scripts/Cores/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.U2D;
 
 public class GameManager : MonoBehaviour
@@ -34,16 +35,67 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    // 히트 버튼
     public void OnHitButton()
     {
+        // 패에 카드 1장 추가
+        PlayerHand.AddCard(_deck.DrawCard());
 
+        // 점수 체크
+        int score = PlayerHand.CalculateScore();
+        Debug.Log($"Player Hit! Score: {score}");
+
+        if (score > 21)
+        {
+            Debug.Log($"Player Bust! 패배");
+            ActionPanel.SetActive(false);
+            // TODO: 리절트 창
+        }
     }
-    
+
+    // 스탠드 버튼
     public void OnStandButton()
     {
+        Debug.Log("Player Stand!");
+        ActionPanel.SetActive(false);
 
+        // 딜러 자동 프로세스 시작
+        StartCoroutine(DealerProcess());
     }
 
+    // 딜러 자동 프로세스
+    IEnumerator DealerProcess()
+    { 
+        // 1초 대기
+        yield return new WaitForSeconds(1.0f);
+
+        // 16점 이하일 시 딜러 드로우
+        while (DealerHand.CalculateScore() <= 16)
+        {
+            DealerHand.AddCard(_deck.DrawCard());
+
+            // 1초 대기
+            yield return new WaitForSeconds(1.0f);
+        }
+
+        CalculateResult();
+    }
+
+    // 리절트 표시
+    void CalculateResult()
+    {
+        int pScore = PlayerHand.CalculateScore();
+        int dScore = DealerHand.CalculateScore();
+
+        Debug.Log($"[Finish] Player: {pScore} vs Dealer: {dScore}");
+
+        if (dScore > 21) Debug.Log("Dealer Bust! Player Win!");
+        else if (pScore > dScore) Debug.Log("Player Win!");
+        else if (pScore < dScore) Debug.Log("Dealer Win...");
+        else Debug.Log("Draw.");
+    }
+
+    // 초기화
     private void Initialize()
     {
         // 판 초기화
