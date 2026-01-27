8257012843613320290b0d2c6e1191e149720f14
Jhlee <ljh990627@gmail.com>
2026-01-27 21:09:05 +0900

    Player 클래스 작성
    Dealer에겐 필요없는 베팅 관련 함수는 전부 Player.cs에

--- Assets/_Scripts/Cores/Player.cs
+++ Assets/_Scripts/Cores/Player.cs
@@ -2,5 +2,37 @@
 
 public class Player : Hand
 {
-    // TODO: 베팅 기능 추가
+    // 현재 소지금
+    public int OwnedMoney { get; private set; } = 1000;
+    // 판돈
+    public int BetAmount { get; private set; } = 0;
+
+    // 베팅
+    public bool Bet(int amount)
+    {
+        // 돈이 부족하면 false
+        if (OwnedMoney < amount) return false;
+
+        OwnedMoney -= amount;
+        BetAmount += amount;
+        return true;
+    }
+
+    // 베팅 초기화
+    public void ResetBet()
+    {
+        OwnedMoney += BetAmount;
+        BetAmount = 0;
+    }
+
+    // 승리 정산
+    public void Win(bool isBlackJack)
+    {
+        // 블랙잭이면 1.5배
+        int profit = isBlackJack ? (int)(BetAmount * 1.5) : BetAmount;
+        OwnedMoney += (BetAmount + profit);
+    }
+
+    // 패배 정산
+    public void Lose() { BetAmount = 0; }
 }
