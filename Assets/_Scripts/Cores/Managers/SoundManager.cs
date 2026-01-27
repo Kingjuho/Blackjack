2961c912b078da1cddcf4e9f7e8a23da58be6ca0
Jhlee <ljh990627@gmail.com>
2026-01-27 18:07:58 +0900

    BGM 재생 테스트
    싱글톤 패턴 사용

---
+++ Assets/_Scripts/Cores/Managers/SoundManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    // 싱글톤
+    public static SoundManager instance;
+
+    [Header("Audio Sources")]
+    [SerializeField] AudioSource _bgmSource; // 배경음악
+    [SerializeField] AudioSource _sfxSource; // 효과음
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // 배경음악 재생
+    public void PlayBGM(AudioClip clip, float volume = 0.5f)
+    {
+        _bgmSource.clip = clip;
+        _bgmSource.volume = volume;
+        _bgmSource.loop = true;
+        _bgmSource.Play();
+    }
+
+    // 효과음 재생
+    public void PlaySFX(AudioClip clip, float volume = 0.5f)
+    {
+        _sfxSource.PlayOneShot(clip, volume);
+    }
+}
