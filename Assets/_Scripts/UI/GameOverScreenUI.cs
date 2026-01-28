3b7a257f98be4303eee493dfc8c63f374541d909
Jhlee <ljh990627@gmail.com>
2026-01-29 00:35:34 +0900

    빌드 세팅 변경 및 버튼 기능 구현

---
+++ Assets/_Scripts/UI/GameOverScreenUI.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreenUI : MonoBehaviour
+{
+    // 타이틀 씬으로 이동
+    public void OnMainButton()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
--- Assets/_Scripts/UI/TitleScreenUI.cs
+++ Assets/_Scripts/UI/TitleScreenUI.cs
@@ -12,10 +12,12 @@ public class TitleScreenUI : MonoBehaviour
     // 에디터면 플레이 모드 종료, 빌드 버전은 진짜 종료
     public void OnExitButton()
     {
-        #if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
-        #else
-            Application.Quit();
-        #endif
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+
     }
 }
