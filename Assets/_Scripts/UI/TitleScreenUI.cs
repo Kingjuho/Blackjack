bdf87f1231602fd87f2b2e4fa55e6b4134405a79
Jhlee <ljh990627@gmail.com>
2026-01-29 00:22:07 +0900

    씬 이름 변경(MainMenui -> TitleScene)
    메인 메뉴보단 타이틀이란 이름이 더 잘어울림

---
+++ Assets/_Scripts/UI/TitleScreenUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenUI : MonoBehaviour
+{
+    // 게임 씬으로 이동
+    public void OnStartButton()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    // 에디터면 플레이 모드 종료, 빌드 버전은 진짜 종료
+    public void OnExitButton()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
