146352502dc01269ccf85b13ebdf0761af5f32f4
Jhlee <ljh990627@gmail.com>
2026-01-29 15:32:17 +0900

    게임 오버 로직 수정
    게임 오버 시 SetStateUI()를 실행하지 않게 해 Retry 버튼을 누를 틈을 주지 않음

--- Assets/_Scripts/States/ResultState.cs
+++ Assets/_Scripts/States/ResultState.cs
@@ -2,7 +2,7 @@
 {
 
     private string _msg;        // 메시지
-    private int _isWin;        // 승리 여부(0: 승리, 1: 무승부, 2: 패배)
+    private int _isWin;         // 승리 여부(0: 승리, 1: 무승부, 2: 패배)
     private bool _isBlackJack;  // 블랙잭 여부
 
     public ResultState(string msg, int isWin, bool isBlackJack = false)
@@ -14,20 +14,25 @@
 
     public void Init(GameManager gm)
     {
-        // UI 변경
-        UIManager.instance.SetStateUI(GameState.Result);
-        UIManager.instance.UpdateResultUI(_msg);
-
         // 승리 시 돈 지급
         if (_isWin == 0) gm.Player.Win(_isBlackJack);
         else if (_isWin == 1) gm.Player.Draw();
         else gm.Player.Lose();
 
+        // 게임 오버 체크
+        if (gm.Player.OwnedMoney <= 0)
+        {
+            gm.ChangeState(new GameOverState());
+            return;
+        }
+        
+        // UI 변경
+        UIManager.instance.SetStateUI(GameState.Result);
+        UIManager.instance.UpdateResultUI(_msg);
+
         // UI 갱신
         gm.UpdateMoneyUI();
 
-        // 게임 오버 체크
-        if (gm.Player.OwnedMoney <= 0) gm.ChangeState(new GameOverState());
     }
 
     public void OnBet(GameManager gm, int amount) { }
